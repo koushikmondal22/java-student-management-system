ee8d4d0d532fac68a8ec98836b55ef731e3b5dde
koushikmondal22 <koumon22@gmail.com>
2026-09-25 09:23:51 +0530

    Create Main.java
    learning java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class main {
+     public static void main(string[] args){
+        System out.println("HELLO JAVA");
+        System out.println("MY NAME IS KOUSHIK");
+        System out.println("I AM LEARNING JAVA FOR MY CAREER");
+    }
+ }
